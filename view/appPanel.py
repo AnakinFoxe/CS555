cdc8d2fdac326c609eedb2cee0b8d9c1949324ff
HU Xing <xing.y.hu@gmail.com>
2013-10-25 18:16:41 -0700

    used more sizers.

--- view/appPanel.py
+++ view/appPanel.py
@@ -10,25 +10,38 @@ class Panel(wx.Panel):
     def __init__(self, parent):
         super(Panel, self).__init__(parent, -1)
 
-        self.sizer = wx.BoxSizer(wx.HORIZONTAL)
-        self.SetSizer(self.sizer)
-        self.SetAutoLayout(True)
-        self.sizer.Fit(self)
+        self.open = wx.Button(self, label="&Open")
+        self.close = wx.Button(self, label="&Close")
 
+        self.sizer_left = wx.BoxSizer(wx.VERTICAL)
+        self.sizer_left.Add(self.open, 0, wx.EXPAND)
+        #self.sizer_left.Add(self.close, 0, wx.EXPAND)
+
+
+        self.sizer_right = wx.BoxSizer(wx.VERTICAL)
+        self.sizer_right.Add(self.close, 0, wx.EXPAND)
+
+
+        self.sizer_all = wx.BoxSizer(wx.HORIZONTAL)
+        self.sizer_all.Add(self.sizer_left, 1, wx.EXPAND)
+        self.sizer_all.Add(self.sizer_right, 1, wx.EXPAND)
+        self.SetSizer(self.sizer_all)
+        self.SetAutoLayout(True)
+        self.sizer_all.Fit(self)
 
     def setLeftImage(self, path):
         self.left_bitmap = wx.Bitmap(path)
         self.left_bitmap = wx.StaticBitmap(self, -1, self.left_bitmap)
 
-        self.sizer.Add(self.left_bitmap, 1, wx.ALIGN_LEFT)
-        self.sizer.Fit(self)
+        self.sizer_left.Add(self.left_bitmap, 1, wx.EXPAND)
+        self.sizer_left.Fit(self)
 
     def setRightImage(self, path):
         self.right_bitmap = wx.Bitmap(path)
         self.right_bitmap = wx.StaticBitmap(self, -1, self.right_bitmap)
 
-        self.sizer.Add(self.right_bitmap, 1, wx.ALIGN_RIGHT)
-        self.sizer.Fit(self)
+        self.sizer_right.Add(self.right_bitmap, 1, wx.EXPAND)
+        self.sizer_right.Fit(self)
 
 
 
