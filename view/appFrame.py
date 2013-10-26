3438238bb1747eae05435eb15aca4700bba2e948
HU Xing <xing.y.hu@gmail.com>
2013-10-26 13:19:50 -0700

    Add StaticBoxSizer to split the window area.

--- view/appFrame.py
+++ view/appFrame.py
@@ -9,4 +9,4 @@ class Frame(wx.Frame):
     def __init__(self):
         wx.Frame.__init__(self, None, -1,
                           title = "Digital Image Processing (CS 555)",
-                          size = (1024, 600))
+                          size = (1000, 740))   # 480 x 640 => (480x2=960) x (640 + 60)
--- view/appPanel.py
+++ view/appPanel.py
@@ -13,35 +13,63 @@ class Panel(wx.Panel):
         self.open = wx.Button(self, label="&Open")
         self.close = wx.Button(self, label="&Close")
 
-        self.sizer_left = wx.BoxSizer(wx.VERTICAL)
-        self.sizer_left.Add(self.open, 0, wx.EXPAND)
-        #self.sizer_left.Add(self.close, 0, wx.EXPAND)
+        """ Left """
+        sizer_left = wx.BoxSizer(wx.VERTICAL)
+
+        """ Left Top """
+        lefttop_title = wx.StaticBox(self, -1, "")
+        self.sizer_lefttop = wx.StaticBoxSizer(lefttop_title, wx.HORIZONTAL)
+        self.sizer_lefttop.Add(self.open, 0, wx.EXPAND)
+
+        """ Left Bottom """
+        leftbtm_title = wx.StaticBox(self, -1, "Original Image")
+        self.sizer_leftbtm = wx.StaticBoxSizer(leftbtm_title, wx.VERTICAL)
+
+        sizer_left.Add(self.sizer_lefttop, 0, wx.EXPAND)
+        sizer_left.Add(self.sizer_leftbtm, 1, wx.EXPAND)
+
+        """ Right """
+        sizer_right = wx.BoxSizer(wx.VERTICAL)
+
+        """ Right Top """
+        righttop_title = wx.StaticBox(self, -1, "")
+        self.sizer_righttop = wx.StaticBoxSizer(righttop_title, wx.HORIZONTAL)
+        self.sizer_righttop.Add(self.close, 0, wx.EXPAND)
+
+        """ Right Center """
+        rightctr_title = wx.StaticBox(self, -1, "Processed Image")
+        self.sizer_rightctr = wx.StaticBoxSizer(rightctr_title, wx.VERTICAL)
+
+        """ Right Bottom """
+        rightbtm_title = wx.StaticBox(self, -1, "")
+        self.sizer_rightbtm = wx.StaticBoxSizer(rightbtm_title, wx.HORIZONTAL)
+
+        sizer_right.Add(self.sizer_righttop, 0, wx.EXPAND)
+        sizer_right.Add(self.sizer_rightctr, 1, wx.EXPAND)
+        sizer_right.Add(self.sizer_rightbtm, 0, wx.EXPAND)
 
 
-        self.sizer_right = wx.BoxSizer(wx.VERTICAL)
-        self.sizer_right.Add(self.close, 0, wx.EXPAND)
-
-
-        self.sizer_all = wx.BoxSizer(wx.HORIZONTAL)
-        self.sizer_all.Add(self.sizer_left, 1, wx.EXPAND)
-        self.sizer_all.Add(self.sizer_right, 1, wx.EXPAND)
-        self.SetSizer(self.sizer_all)
+        """ All """
+        sizer_all = wx.BoxSizer(wx.HORIZONTAL)
+        sizer_all.Add(sizer_left, 1, wx.EXPAND)
+        sizer_all.Add(sizer_right, 1, wx.EXPAND)
+        self.SetSizer(sizer_all)
         self.SetAutoLayout(True)
-        self.sizer_all.Fit(self)
+        sizer_all.Fit(self)
 
     def setLeftImage(self, path):
         self.left_bitmap = wx.Bitmap(path)
         self.left_bitmap = wx.StaticBitmap(self, -1, self.left_bitmap)
 
-        self.sizer_left.Add(self.left_bitmap, 1, wx.EXPAND)
-        self.sizer_left.Fit(self)
+        self.sizer_leftbtm.Add(self.left_bitmap, 1, wx.ALIGN_CENTER)
+        self.sizer_leftbtm.Fit(self)
 
     def setRightImage(self, path):
         self.right_bitmap = wx.Bitmap(path)
         self.right_bitmap = wx.StaticBitmap(self, -1, self.right_bitmap)
 
-        self.sizer_right.Add(self.right_bitmap, 1, wx.EXPAND)
-        self.sizer_right.Fit(self)
+        self.sizer_rightctr.Add(self.right_bitmap, 1, wx.ALIGN_CENTER)
+        self.sizer_rightctr.Fit(self)
 
 
 
